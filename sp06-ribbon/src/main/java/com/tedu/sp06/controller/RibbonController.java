72b36a9fae1419b5bd5029c7237a1b3ae42d9952
tarena <tarena@tarena-pc>
2019-11-01 16:09:25 +0800

    bug修改

--- sp06-ribbon/src/main/java/com/tedu/sp06/controller/RibbonController.java
+++ sp06-ribbon/src/main/java/com/tedu/sp06/controller/RibbonController.java
@@ -18,35 +18,35 @@ import com.tedu.web.util.JsonResult;
 @RestController
 public class RibbonController {
 	@Autowired
-	private RestTemplate rt;
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/item-service/{orderId}")
 	public JsonResult<List<Item>> getItems(@PathVariable String orderId){
-		return rt.getForObject("http://item-service/{1}", JsonResult.class, orderId);
+		return restTemplate.getForObject("http://item-service/{1}", JsonResult.class, orderId);
 	} 
 	@PostMapping("/item-service/decreaseNumber")
 	public JsonResult decreaseNumber(@RequestBody List<Item> items) {
-		return rt.postForObject("http://item-service/decreaseNumber", items, JsonResult.class);
+		return restTemplate.postForObject("http://item-service/decreaseNumber", items, JsonResult.class);
 	}
 	
 	@GetMapping("/user-service/{userId}")
 	public JsonResult<User> getUser(@PathVariable Integer userId) {
-		return rt.getForObject("http://user-serivce/{1}", JsonResult.class, userId);
+		return restTemplate.getForObject("http://user-serivce/{1}", JsonResult.class, userId);
 	}
 
 	@GetMapping("/user-service/{userId}/score")
 	public JsonResult addScore(
 			@PathVariable Integer userId, Integer score) {
-		return rt.getForObject("http://user-serivce/{1}/score?score={2}", JsonResult.class, userId, score);
+		return restTemplate.getForObject("http://user-serivce/{1}/score?score={2}", JsonResult.class, userId, score);
 	}
 	
 	@GetMapping("/order-service/{orderId}")
 	public JsonResult<Order> getOrder(@PathVariable String orderId) {
-		return rt.getForObject("http://order-service/{1}", JsonResult.class, orderId);
+		return restTemplate.getForObject("http://order-service/{1}", JsonResult.class, orderId);
 	}
 	
 	@GetMapping("/order-service/")
 	public JsonResult addOrder() {
-		return rt.getForObject("http://order-service/", JsonResult.class);
+		return restTemplate.getForObject("http://order-service/", JsonResult.class);
 	}
 }
