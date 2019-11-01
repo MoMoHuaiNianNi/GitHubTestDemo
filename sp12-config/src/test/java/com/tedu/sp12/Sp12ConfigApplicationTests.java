efe21350b2d1ee482eca14da09958c22a5cbd82a
tarena <tarena@tarena-pc>
2019-11-01 17:25:49 +0800

    daima tijiao

---
+++ sp12-config/src/test/java/com/tedu/sp12/Sp12ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.sp12;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp12ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
