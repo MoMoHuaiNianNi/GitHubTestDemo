5a1f3324c257928e5083e1eec4e68bcd9dd42255
tarena <tarena@tarena-pc>
2019-11-04 09:47:03 +0800

    配置刷新测试

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.tedu.sp03.user.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,31 +12,32 @@ import com.tedu.sp01.service.UserService;
 import com.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
+@RefreshScope
 public class UserServiceImpl implements UserService {
 
 	@Value("${sp.user-service.users}")
 	private String userJson;
-	
+
 	@Override
 	public User getUser(Integer id) {
-		List<User> list = 
-		 JsonUtil.from(userJson,
-		 new TypeReference<List<User>>() {});
-		
+		List<User> list = JsonUtil.from(userJson, new TypeReference<List<User>>() {
+		});
+
 		for (User user : list) {
 			if (user.getId().equals(id)) {
 				return user;
 			}
 		}
-		
-		return new User(id, "username"+id, "password"+id);
+
+		return new User(id, "username" + id, "password" + id);
 	}
 
 	@Override
 	public void addScore(Integer id, Integer score) {
-		log.info("user - "+id+", score - "+score);
+		log.info("user - " + id + ", score - " + score);
 	}
 
 }
